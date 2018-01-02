1274a4dd7d6bba410cddbdf48104722d5d59736f
LourdesLoayza <30554218+LourdesLoayza@users.noreply.github.com>
2018-01-01 20:38:21 -0500

    searching restaurants

--- js/app.js
+++ js/app.js
@@ -1,36 +1,8 @@
 
 
-// Búsqueda de locales
-
-function begin() {
-  search();
-}
-
-function search() {
-
-  var selectDistrict = $('#select-district')
-  var district;
-
- 
-  function selectionDistrict(event) {
-
-    
-    switch (true) {
-     
-      case event.target.value === 'LIMA':
-        district = data['LIMA'];
-        console.log('PRUEBA')
-        break;
-        case event.target.value === 'MIRAFLORES':
-        district = data['MIRAFLORES'];
-        console.log('PRUEBA')
-        break;
-        case event.target.value === 'SAN-ISIDRO':
-        district = data['SAN-ISIDRO'];
-        console.log('PRUEBA')
-        break;
-    }
-  }
-}
-
-$(document).ready(begin);
+$(document).ready(function() {
+  // Redireccionando home
+  setTimeout(function() {
+    window.location.href = 'views/search.html';
+  }, 3500);
+});
